5bf7142d624544ef8aab3a253b321349d7804918
Luke Heidecke <luke@solinea.com>
2015-06-24 17:38:32 -0700

    Uncommented the link command

---
+++ entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! $APPDIR ]; then export APPDIR=./; fi
+if [ ! $RERUN_MODULES ]; then export RERUN_MODULES=./modules; fi
+
+function create-module-links {
+  echo "Creating module links..."
+  for module in $APPDIR/modules/*; do
+    if [ -d $module ]; then
+      module_name="$(basename "${module}")"
+      ln -sf $module $RERUN_MODULES/$module_name
+    fi
+  done
+}
+
+# if empty, display versions of all modules
+if [[ $# -eq 0 ]]; then
+  rerun
+  exit 0
+fi
+
+# parse for options
+while [ "$1" != "" ]; do
+  case $1 in
+    -s|--shell)
+      create-module-links
+      echo "Starting interactive shell..."
+      bash
+      exit 0
+      ;;
+    *)
+      create-module-links
+      rerun $1
+      ;;
+  esac
+  shift
+done
